dcf65a0b03133578be101142676142693fe2706c
Joey <sjferenchak@gmail.com>
2019-12-04 22:50:49 -0500

    added installation file

---
+++ setup.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+#extract files
+
+echo "PLEASE NOTE: This script will overwrite files in your home directory. Backup is recommended."
+echo "Continue installation? (y/n)"
+read input
+
+if [[ $input == y ]] || [[ $input == yes ]]; then
+  rsync -avz $PWD/ $HOME --exclude /.git --exclude /README.md --exclude /setup.sh
+  echo "Installation complete, exting script"
+else
+  echo "Exiting script"
+fi
